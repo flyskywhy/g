92068e1ff2f227742b64af302c5e3fd756f69c87
Li Zheng <flyskywhy@gmail.com>
2023-06-03 16:26:01 +0800

    行业类型.mm: 参考自一些网页上的行业提问下拉框

---
+++ i主观的体验方式/t快乐的体验/商/行业类型.mm
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1438694198" 
+	TEXT="行业类型">
+<node FOLDED="true" ID="ID_1337459910" POSITION="right" 
+	TEXT="互联网与信息技术">
+<node 
+	TEXT="基础和工具软件"/>
+<node 
+	TEXT="垂直行业应用"/>
+<node 
+	TEXT="企业管理软件"/>
+<node 
+	TEXT="人工智能、大数据、物联网"/>
+<node 
+	TEXT="游戏"/>
+<node 
+	TEXT="信息安全"/>
+<node 
+	TEXT="营销与增长"/>
+<node 
+	TEXT="IT基础设施与运维服务"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node POSITION="right" 
+	TEXT="通信"/>
+<node FOLDED="true" ID="ID_163557385" POSITION="right" 
+	TEXT="零售与电商">
+<node 
+	TEXT="电商"/>
+<node 
+	TEXT="垂直品类零售"/>
+<node 
+	TEXT="实体商户"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_598173558" POSITION="right" 
+	TEXT="医疗和健康">
+<node 
+	TEXT="制药"/>
+<node 
+	TEXT="生物科技"/>
+<node 
+	TEXT="医疗服务"/>
+<node 
+	TEXT="医疗设备"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_1099296616" POSITION="right" 
+	TEXT="汽车">
+<node 
+	TEXT="新能源汽车"/>
+<node 
+	TEXT="传统汽车"/>
+<node 
+	TEXT="汽车零部件制造"/>
+<node 
+	TEXT="智能汽车技术"/>
+<node 
+	TEXT="汽车服务"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_624230981" POSITION="right" 
+	TEXT="消费电子及家电">
+<node 
+	TEXT="手机和数码产品"/>
+<node 
+	TEXT="家电"/>
+<node 
+	TEXT="芯片和半导体"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_1252950338" POSITION="right" 
+	TEXT="文娱传媒及社交">
+<node 
+	TEXT="社交媒体"/>
+<node 
+	TEXT="短视频和直播"/>
+<node 
+	TEXT="文化传媒"/>
+<node 
+	TEXT="影视、综艺及娱乐"/>
+<node 
+	TEXT="广告营销"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_610997228" POSITION="right" 
+	TEXT="金融">
+<node 
+	TEXT="银行"/>
+<node 
+	TEXT="保险"/>
+<node 
+	TEXT="资本市场"/>
+<node 
+	TEXT="消费金融"/>
+<node 
+	TEXT="金融科技"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_467421571" POSITION="right" 
+	TEXT="教育">
+<node 
+	TEXT="学校"/>
+<node 
+	TEXT="在线教育"/>
+<node 
+	TEXT="知识付费"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_368693059" POSITION="right" 
+	TEXT="建筑与房地产">
+<node 
+	TEXT="建筑工程"/>
+<node 
+	TEXT="房地产服务"/>
+<node 
+	TEXT="物业服务"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node FOLDED="true" ID="ID_1493350668" POSITION="right" 
+	TEXT="快消及鞋服">
+<node 
+	TEXT="鞋服家纺"/>
+<node 
+	TEXT="美妆个护"/>
+<node 
+	TEXT="母婴用品"/>
+<node 
+	TEXT="家居清洁"/>
+<node 
+	TEXT="食品饮料"/>
+<node 
+	TEXT="其他"/>
+</node>
+<node ID="ID_1645900843" POSITION="right" 
+	TEXT="能源"/>
+<node FOLDED="true" ID="ID_180917195" POSITION="right" 
+	TEXT="政府及公共事业">
+<node 
+	TEXT="政府机构"/>
+<node 
+	TEXT="事业单位"/>
+<node 
+	TEXT="公益组织"/>
+<node 
+	TEXT="其他社会组织"/>
+</node>
+<node POSITION="right" 
+	TEXT="专业服务"/>
+<node POSITION="right" 
+	TEXT="其他"/>
+</node>
+</map>
