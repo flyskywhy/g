3613d27916c2c297d0d4a3cb34833fcef6b00d50
Li Zheng <flyskywhy@gmail.com>
2023-07-04 15:18:58 +0800

    行业主管部门.mm: 参考自一些网页上的提问下拉框

---
+++ i主观的体验方式/t快乐的体验/商/行业主管部门.mm
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1635415110" 
+	TEXT="行业主管部门">
+<node ID="ID_590949610" POSITION="right" 
+	TEXT="某某市委组织部"/>
+<node ID="ID_1349396097" POSITION="right" 
+	TEXT="某某市委宣传部"/>
+<node ID="ID_1183874883" POSITION="right" 
+	TEXT="某某市农业农村局"/>
+<node ID="ID_665934876" POSITION="right" 
+	TEXT="某某市地方金融监管局"/>
+<node ID="ID_1803116803" POSITION="right" 
+	TEXT="某某市经信局"/>
+<node ID="ID_855326615" POSITION="right" 
+	TEXT="某某市市场监管局"/>
+<node ID="ID_168515729" POSITION="right" 
+	TEXT="某某市文化广电旅游局"/>
+<node ID="ID_896823155" POSITION="right" 
+	TEXT="某某市建委"/>
+<node ID="ID_755461054" POSITION="right" 
+	TEXT="某某市卫健委"/>
+<node ID="ID_87352349" POSITION="right" 
+	TEXT="某某市体育局"/>
+<node ID="ID_1392887948" POSITION="right" 
+	TEXT="某某市社科联"/>
+<node ID="ID_987797567" POSITION="right" 
+	TEXT="某某市教育局"/>
+<node ID="ID_1810739359" POSITION="right" 
+	TEXT="某某市人力社保局"/>
+<node ID="ID_1890147403" POSITION="right" 
+	TEXT="某某市科技局"/>
+<node ID="ID_183340868" POSITION="right" 
+	TEXT="某某市总工会"/>
+<node ID="ID_1278241142" POSITION="right" 
+	TEXT="共青团某某市委"/>
+<node ID="ID_463355533" POSITION="right" 
+	TEXT="某某市文联"/>
+<node ID="ID_433535037" POSITION="right" 
+	TEXT="某某市科协"/>
+<node ID="ID_237206952" POSITION="right" 
+	TEXT="市退役军人事务局"/>
+</node>
+</map>
